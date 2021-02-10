28c78ffe75ed033bfe1417e575506e7f7fad0796
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2021-02-10 19:47:27 +0100

    Update settings.py

--- WalletAPI/settings.py
+++ WalletAPI/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import dj_database_url
 import django_heroku
 from decouple import config
 
@@ -81,10 +82,8 @@ WSGI_APPLICATION = 'WalletAPI.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True,
+                                      default=config('DATABASE_URL'))
 }
 
 # Password validation
